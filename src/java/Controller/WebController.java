015d5cdc750e10b32bd6c49e54049269db4a0ea5
Your Name <you@example.com>
2020-04-13 15:16:43 +0300

    Продолжение Web-controller есть ошибки

--- src/java/Controller/WebController.java
+++ src/java/Controller/WebController.java
@@ -18,11 +18,17 @@ import javax.servlet.http.HttpServletResponse;
  * @author User
  */
 @WebServlet(name = "WebController", urlPatterns = {
-    "/WebController"
+    "/showAddBook",
+    "/createBook",
 })
 public class WebController extends HttpServlet {
         protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
             response.setContentType("text/html;charset=UTF-8");
+            request.setCharacterEncoding("UTF-8");
+            String path = request.getServletPath();
+            switch (path){
+                case "/showAddBook";
+            }
         }
                 
     
